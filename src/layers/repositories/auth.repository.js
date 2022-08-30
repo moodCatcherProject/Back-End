a2e28b172f22587edbe55ab24c70c5c685d4cc35
subeomlee <dltnqja9@naver.com>
2022-08-31 01:12:04 +0900

    로컬로그인 기능구현 중간저장

--- src/layers/repositories/auth.repository.js
+++ src/layers/repositories/auth.repository.js
@@ -1,1 +1,13 @@
-//abc
+const User = require("../../../models/user");
+const bcrypt = require("bcrypt");
+
+const findByEmail = async (email) => {
+    await User.findOne({ where: { email } });
+};
+
+const createSignUp = async (email, nickname, password) => {
+    const hash = await bcrypt.hash(password, 12);
+    await User.Create({ email, nickname, password: hash });
+};
+
+module.exports = { findByEmail, createSignUp };
--- src/layers/services/auth.service.js
+++ src/layers/services/auth.service.js
@@ -1,5 +1,26 @@
-const authRepository = require("../repositories/auth.repository")
+const authRepository = require("../repositories/auth.repository");
+const Exception = require("../exceptModels/_.models.loader");
+const joi = require("joi");
 
-module.exports = {
-    
-}
+const localSignUp = async (email, nickname, password, confirmPw) => {
+    const ExistUser = await authRepository.findByEmail(email);
+    if (ExistUser) {
+        return res.redirect("/signup?error=exist");
+    }
+    // 이메일 타입 확인
+    // 비밀번호 정규식 확인
+    if (password !== confirmPw) {
+        return { msg: "비번 비번확인 다름" }; // 임시
+    }
+    if (email === ExistUser) {
+        return { msg: "이멜 중복" }; // 임시
+    }
+    const SignUp = await authRepository.createSignUp(email, nickname, password);
+    return SignUp;
+};
+
+module.exports = { localSignUp };
+
+// EM :8자~ 30자
+// PW :영소대문자+숫자+특수문자 8자 ~ 20자
+// NN : 한글, 영소 대문자. 숫자 2자~16자
