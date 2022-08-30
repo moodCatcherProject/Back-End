a6162a667a8b4796b89d50eac530e771dd849f5f
gwonyeong <qwe980121@naver.com>
2022-08-31 01:27:33 +0900

    폴더구조 최종 변경

--- src/layers/_router.loader.js
+++ src/layers/_router.loader.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-
+const {isLoggedIn, isNotLoggedIn} = require("./routes/middlewares/authMiddle")
 const searchRouter = require("./routes/Item.router");
 const authRouter = require("./routes/auth.router");
+const postRouter = require("./routes/post.router")
 
 router.use("/search", searchRouter);
 router.use("/auth", authRouter);
+router.use("/", isLoggedIn , postRouter);
 
 module.exports = router;
