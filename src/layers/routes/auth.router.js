db4db1d37979f484e157bc5218eb700a2d293f24
subeomlee <dltnqja9@naver.com>
2022-09-01 01:04:32 +0900

    닉네임 나이 성별 기능 에러 수정

--- src/layers/routes/auth.router.js
+++ src/layers/routes/auth.router.js
@@ -6,7 +6,7 @@ const AuthController = require("../controllers/auth.controller");
 
 // 회원가입 , URL : /api/auth/signup
 authRouter.post("/signup", AuthController.localSignUp);
-// 닉네임/나이 추가 , URL : /api/auth/detail
+// 닉네임/나이/성별 추가 , URL : /api/auth/detail
 authRouter.post("/detail", AuthController.createNicknameAgeGender);
 // 이메일 확인 , URL : /api/auth?email
 authRouter.get("/", AuthController.checkEmail);
