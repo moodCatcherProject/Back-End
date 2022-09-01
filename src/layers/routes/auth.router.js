1bc2170183dba1732e0a2f35b43bde7eb101bd3d
sumin-dev <bonar8888@naver.com>
2022-09-01 16:47:02 +0900

    feature auth API
    로컬로그인 세션방식으로 변경, 수범님 파일 병합

--- src/layers/routes/auth.router.js
+++ src/layers/routes/auth.router.js
@@ -2,17 +2,22 @@ const express = require("express");
 const authRouter = express.Router();
 const authController = require("../controllers/auth.controller");
 const passport = require("passport");
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares/authMiddle');
 
-authRouter.route("/login").post(authController.localLogin);
 
-// 회원가입 , URL /api/auth/signup
+// 회원가입(/api/auth/signup)
 authRouter.post("/signup", authController.localSignUp);
-// // 이메일 확인 , URL /api/auth?email
-// authRouter.get("/", authController.checkEmail);
-// // 닉네임 확인 , URL /api/auth?nickname
-// authRouter.get("/", authController.checkNickname);
+// // 이메일 확인(/api/auth/checkEmail?email)
+// authRouter.get("/checkEmail", authController.checkEmail);
+// // 닉네임 확인(/api/auth/checkNickname?nickname)
+// authRouter.get("/checkNickname", authController.checkNickname);
+// 닉네임/나이/성별 추가(/api/auth/detail)
+authRouter.post("/detail", authController.createNicknameAgeGender);
 
-//카카오 로그인
+// 로컬로그인(/api/auth/login)
+authRouter.post("/login", authController.localLogin);
+
+//카카오 로그인(/api/auth/kakao)
 authRouter.get("/kakao", passport.authenticate("kakao"));
 
 authRouter.get(
