7adc49ef5fe4c8a0e63d729d8bcab95bd14464e7
sumin-dev <bonar8888@naver.com>
2022-09-02 12:04:32 +0900

    husky test

--- src/layers/routes/user.router.js
+++ src/layers/routes/user.router.js
@@ -1,14 +1,13 @@
-const express = require("express");
+const express = require('express');
 const userRouter = express.Router();
-const exception = require("../exceptModels/_.models.loader");
+const exception = require('../exceptModels/_.models.loader');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares/authMiddle');
 
-
 // 로그아웃(api/user/logout)
-userRouter.get("/logout", isLoggedIn, (req, res) => {req.logout(); req.session.destroy();
-    res.status(200).json(
-        new exception.FormDto("로그아웃 성공")
-    );
-})
+userRouter.get('/logout', isLoggedIn, (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.status(200).json(new exception.FormDto('로그아웃 성공'));
+});
 
 module.exports = userRouter;
