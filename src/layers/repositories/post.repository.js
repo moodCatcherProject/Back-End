4075a725e424698a2eeb3d4067977a2ec5d4bdf6
sumin-dev <bonar8888@naver.com>
2022-09-27 16:34:38 +0900

    Fix: change order in findHonorPosts API

--- src/layers/repositories/post.repository.js
+++ src/layers/repositories/post.repository.js
@@ -535,7 +535,10 @@ const findHonorPosts = async (page, count) => {
             limit: count,
             distinct: true,
             attributes: { exclude: ['honorId'] },
-            order: [['honorId', 'DESC']],
+            order: [
+                ['createdAt', 'DESC'],
+                ['rank', 'ASC']
+            ],
             include: [
                 {
                     model: Post,
